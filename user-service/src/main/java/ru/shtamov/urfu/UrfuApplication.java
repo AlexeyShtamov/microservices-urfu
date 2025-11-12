aafaf67ed13f98a1de3224b4235c4d093ba612f6
Aleksey Shtamov <a.shtamov@corp.mail.ru>
2025-11-12 22:57:25 +0500

    first commit

---
+++ user-service/src/main/java/ru/shtamov/urfu/UrfuApplication.java
@@ -0,0 +1,13 @@
+package ru.shtamov.urfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrfuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrfuApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/ru/shtamov/urfu/UrfuApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shtamov.urfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrfuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
